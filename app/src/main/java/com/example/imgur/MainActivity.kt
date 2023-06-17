62d0ca7aec1a9a14e46b7170be1e6d1d062f8898
taiga945 <olaopg@gmail.com>
2023-06-17 14:25:22 +0200

    First try of using compose

--- app/src/main/java/com/example/imgur/MainActivity.kt
+++ app/src/main/java/com/example/imgur/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.imgur
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.imgur.ui.theme.ImgurTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +35,105 @@ class MainActivity : ComponentActivity() {
         setContent {
             ImgurTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                Screen()
             }
         }
+    }
+}
+@Composable
+fun Screen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background)
+    ) {
+        val openDialog = remember { mutableStateOf(true) }
+        val textState = remember {
+            mutableStateOf("Android")
+        }
+        Greeting(textState.value)
+        Button(
+            onClick = {
+                if (textState.value != "Ola") {
+                    textState.value = "Ola"
+                } else {
+                    textState.value = "Android"
+                }
+                openDialog.value = true
+            },
+            modifier = Modifier.wrapContentSize(),
+        ) {
+            Text("change")
+        }
+        Dialog(openDialog = openDialog)
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    if (name == "Ola") {
+        Text(text = "Maciek")
+    } else {
+        Text(
             text = "Hello $name!",
             modifier = modifier
-    )
+        )
+    }
+}
+
+@Composable
+fun Dialog(openDialog: MutableState<Boolean>) {
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                openDialog.value = false
+            },
+            confirmButton = {
+                Surface(
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .wrapContentHeight(),
+                    shape = MaterialTheme.shapes.large,
+                    tonalElevation = AlertDialogDefaults.TonalElevation
+                ) {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Text(
+                            text = "This area typically contains the supportive text " +
+                                    "which presents the details regarding the Dialog's purpose.",
+                        )
+                        Spacer(modifier = Modifier.height(24.dp))
+                        TextButton(
+                            onClick = {
+                                openDialog.value = false
+                            },
+                            modifier = Modifier.align(Alignment.End)
+                        ) {
+                            Text("Confirm")
+                        }
+                    }
+                }
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScreenPreview() {
     ImgurTheme {
-        Greeting("Android")
+        Screen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DialogPreview() {
+    ImgurTheme {
+        Dialog(remember {
+            mutableStateOf(true)
+        })
     }
 }
